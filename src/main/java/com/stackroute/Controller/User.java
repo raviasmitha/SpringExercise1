e02b0027cf933be6e4da29ad3db79b55b4e95c7f
raviasmitha <asmithachowdary1511@gmail.com>
2019-07-16 15:40:14 +0530

    first commit

---
+++ src/main/java/com/stackroute/Controller/User.java
@@ -0,0 +1,26 @@
+package com.stackroute.Controller;
+
+
+public class User
+{
+  String name;
+
+  public User(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "name='" + name + '\'' +
+      '}';
+  }
+}
